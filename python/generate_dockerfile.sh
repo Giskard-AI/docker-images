330dc96918fdf7e6e209cd9ddb32a71f7e0e0b7d
Bazire Houssin <hartorn.github@gmail.com>
2025-06-09 11:02:06 +0200

    updating ubuntu

--- python/generate_dockerfile.sh
+++ python/generate_dockerfile.sh
@@ -7,7 +7,7 @@ FOLDER=$(readlink -f "${BASH_SOURCE[0]}" | xargs dirname)
 DEBIAN_VERSION="bookworm"
 DEBIAN_VARIANT="slim"
 # Should be updated regularly, to endforce most recent ubuntu
-UBUNTU_BASE_IMAGE="ubuntu:24.04@sha256:6015f66923d7afbc53558d7ccffd325d43b4e249f41a6e93eef074c9505d2233"
+UBUNTU_BASE_IMAGE="ubuntu:24.04@sha256:b59d21599a2b151e23eea5f6602f4af4d7d31c4e236d22bf0b62b86d2e386b8f"
 
 function install_python() {
     output_file=$1
